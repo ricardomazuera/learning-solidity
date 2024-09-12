9e35a09759f998cfd16b1a08eb62b07d34bb98da
Ricardo Mazuera <ricardo.mazuera@gmail.com>
2024-09-11 21:31:31 -0500

    added first smart contract SimpleStorage

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# learning-solidity
+# Learning Solidity
+
+## Introduction
+In this repo I will be documenting my journey learning Solidity. I will be using the [Cyfrin Updraft](https://updraft.cyfrin.io/) courses as a guide. I will also be using the [Remix IDE](https://remix.ethereum.org/) to write and deploy my smart contracts.
+
+## Table of Contents
+1. Solidity Smart Contract Development
+    It directory contains the code and notes for the Cyfrin Updraft course on [Solidity Smart Contract Development](https://updraft.cyfrin.io/courses/solidity).
---
+++ Solidity Smart Contract Development/SimpleStorage.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19; // solidity version
+
+// here we creating a contract
+contract SimpleStorage {
+    // Basic Types: boolean, uint, int, address, bytes
+
+    // Bool
+    // bool hasFavoriteNumber = true;
+
+    // uint
+    // in uint and int we can specify how many bits we want 
+    // that they use.
+    // If you don't specify bits, default is 256
+    //uint256 favoriteNumber = 88;
+
+    // we can only declarate the variable and solidity sets
+    // this according with the type. For e.g:
+    uint256 myFavoriteNumber; // uint256 is 0 for standard
+
+    // lists
+    // uint256[] listOfFavoriteNumbers; // [0,10,25,85]
+
+    // structs
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    /*
+    Person public pat = Person({       
+        favoriteNumber: 7, 
+        name: "Pat"    
+    });
+    */
+
+    // creating list of persons
+    // static array
+    // Person[3] listOfPeople;
+    Person[] public listOfPeople;
+
+    // int
+    // int256 favoriteInt = -88;
+
+    // string
+    // string favoriteNumberText = "eighty eight";
+
+    // address
+    // address myAddress = 0x589605619b607C9eBF8520ce75D3007F4Dafd3d9;
+
+    // bytes
+    // bytes32 favoriteBytes32 = "cat";
+
+    // mapping
+    mapping (string => uint256) public nameToFavoriteNumber;
+
+    // we'll create to new function
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    // view, pure
+    // solidity have these two words when we only going to
+    // read the state of the blockchain and don't write in this
+    function retrieveFavoriteNumber() public view returns(uint256){
+        return myFavoriteNumber;
+    }
+
+    function addPerson(uint256 _favoriteNumber, string memory _name) public {
+        // We can create a newPerson to then pass it to listOfPeople
+        // Person memory newPerson = Person(_favoriteNumber, _name);
+        
+        // However, we can pass directly the newPerson to the listOfPeople
+        listOfPeople.push(Person(_favoriteNumber,_name));
+
+        // Pass _favoriteNumber to mapping nameToFavoriteNumber
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
